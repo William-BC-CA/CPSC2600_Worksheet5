ea348ccbb64edb27d9ce81e296a2e167c1f76852
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-10-16 01:02:38 +0000

    finish (i believe)

---
+++ my-app/src/styles/theme.js
@@ -0,0 +1,24 @@
+export default {
+    lightTheme: {
+        colors: {
+            background: 'radial-gradient(grey, black)', // Don't know why it was lightGrey by default
+            black: 'black',
+            white: 'white',
+            blue: '#a0e9fd',
+            lightBlue: '#caf3fe',
+            secondary: 'radial-gradient(green, yellow)',
+        },
+        transition: '0.3s',
+    },
+    darkTheme: {
+        colors: {
+            background: 'radial-gradient(black, grey)', // Don't know why it was lightGrey by default
+            black: 'white',
+            white: 'black',
+            blue: '#a0e9fd',
+            lightBlue: '#caf3fe',
+            secondary: 'radial-gradient(yellow, green)',
+        },
+        transition: '0.3s',
+    },
+}
